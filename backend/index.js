9afcec273743c99bff074b205726d56727f32c1a
Suraj Jagtap <suraj552003@gmail.com>
2024-01-18 10:27:08 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": "305cf472-13a1-47a1-949a-862d885e2c9e" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
